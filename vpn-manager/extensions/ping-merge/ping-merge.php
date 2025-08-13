933fa640cce588202c7f1d22e4898e6caa3662d0
ebmohsenn <mohsen.ebrahimizadeh@hotmail.com>
2025-08-13 15:43:27 +0200

    de

--- vpn-manager/extensions/ping-merge/ping-merge.php
+++ vpn-manager/extensions/ping-merge/ping-merge.php
@@ -19,5 +19,20 @@ add_action('init', function(){
 
 add_action('admin_enqueue_scripts', function($hook){
     if ($hook !== 'toplevel_page_hovpnm') return;
-    wp_add_inline_script('jquery-core', 'jQuery(function($){ $(document).on("click", ".button[data-action=ping]", function(e){ e.preventDefault(); var b=$(this), id=b.data("id"); b.prop("disabled", true); var d1=$.Deferred(), d2=$.Deferred(); $.post(ajaxurl,{action:"hovpnm_srv_ping", id:id, _ajax_nonce:HOVPNM_SP?HOVPNM_SP.nonce:""}, function(){ d1.resolve(); }).fail(function(){ d1.resolve(); }); $.post(ajaxurl,{action:"hovpnm_ch_ping", id:id, force:1, _ajax_nonce:HOVPNM_CH?HOVPNM_CH.nonce:""}, function(){ d2.resolve(); }).fail(function(){ d2.resolve(); }); $.when(d1,d2).always(function(){ b.prop("disabled", false); }); }); });');
+        $script = <<<'JS'
+jQuery(function($){
+    $(document).on('click', '.button[data-action="ping"]', function(e){
+        e.preventDefault();
+        var b=$(this), id=b.data('id');
+        b.prop('disabled', true);
+        var d1=$.Deferred(), d2=$.Deferred();
+        var spNonce = (window.HOVPNM_SP && HOVPNM_SP.nonce) ? HOVPNM_SP.nonce : '';
+        var chNonce = (window.HOVPNM_CH && HOVPNM_CH.nonce) ? HOVPNM_CH.nonce : '';
+        $.post(ajaxurl, { action:'hovpnm_srv_ping', id:id, _ajax_nonce: spNonce }, function(){ d1.resolve(); }).fail(function(){ d1.resolve(); });
+        $.post(ajaxurl, { action:'hovpnm_ch_ping', id:id, force:1, _ajax_nonce: chNonce }, function(){ d2.resolve(); }).fail(function(){ d2.resolve(); });
+        $.when(d1,d2).always(function(){ b.prop('disabled', false); });
+    });
+});
+JS;
+        wp_add_inline_script('jquery', $script);
 });
